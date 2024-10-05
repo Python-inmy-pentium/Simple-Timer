c360e973ea62eafee2bf29d3fbda2fec7d0b2ec6
Python-inmy-pentium <l.jah.entr@gmail.com>
2024-10-05 23:34:59 +0100

    Update

--- timer + alarm + UI.py
+++ timer + alarm + UI.py
@@ -1,13 +1,36 @@
 import PySimpleGUI as sg
 from time import sleep
 
-layout = [[sg.InputText(size=(2, 3), key="mins", default_text="00", ), sg.Text(":"),
-           sg.InputText(size=(2, 3), key="secs", default_text="00")],
-          [sg.Button("Start", font=(30, 4), mouseover_colors="green", pad=((80, 0), (0, 0)), visible=True)]]
-window = sg.Window("Timer", layout, scaling=10, location=(0, 0))
+
+def timer_2_up():
+    mins = values["mins"]
+    secs = values["secs"]
+    print(mins, ":", secs)
+    #  Using sleep and divmod
+    m = int(mins)
+    s = int(secs)
+    total = (m * 60) + s
+    # print(total), for dev's use only
+    times = divmod(total, 60)
+    while True:
+        if times[0] == times[1] == 0:
+            print("The End!")
+            alarm()
+            window["Start"].update(visible=True)
+            break
+        else:
+            total -= 1
+            times = divmod(total, 60)
+            print("%02d" % times[0], ":", "%02d" % times[1])
+            minute = "%02d" % times[0]
+            second = "%02d" % times[1]
+            sleep(0.5)
+            window["mins"].update(minute)
+            window["secs"].update(second)
+            window.refresh()  # Very important entity
+            sleep(0.5)
 
 
-# Alarm function that uses the pygame module
 def alarm():
     import pygame
     pygame.mixer.init()
@@ -17,37 +40,26 @@ def alarm():
     pygame.mixer_music.stop()
 
 
+layout = [
+    [
+        sg.Input("00", size=(2, 5), font=(100, 60), key="mins", enable_events=True),
+        sg.Text(":", font=(100, 60), key="hype"),
+        sg.InputText(size=(2, 5), font=(100, 60), key="secs", default_text="00")
+    ],
+    [
+        sg.Button("Start", font=(100, 30), mouseover_colors="green", pad=((50, 0), (0, 0)), visible=True),
+    ]
+]
+window = sg.Window("Timer", layout, location=(0, 0), keep_on_top=True, grab_anywhere=True)
+
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
 
     if event == "Start":
-        mins = values["mins"]
-        secs = values["secs"]
-        print(mins, ":", secs)
-        #  Using sleep and divmod
-        m = int(mins)
-        s = int(secs)
-        total = (m * 60) + s
-        # print(total), for dev's use only
-        times = divmod(total, 60)
-        while True:
-            if times[0] == times[1] == 0:
-                print("The End!")
-                alarm()
-                break
-            else:
-                total -= 1
-                times = divmod(total, 60)
-                print("%02d" % times[0], ":", "%02d" % times[1])
-                minute = "%02d" % times[0]
-                second = "%02d" % times[1]
-                sleep(0.5)
-                window["mins"].update(minute)
-                window["secs"].update(second)
-                window.refresh()  # Very important entity
-                sleep(0.5)
-
+        window["Start"].update(visible=False)
+        timer_2_up()
 
 window.close()
